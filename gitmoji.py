94cf63257dab77ffd4776da827aa6d77ab041db3
Paul Hendrick <paul.g.hendrick@gmail.com>
2018-03-17 14:18:02 -0400

    ✨ Implement matching icons for each item
    This workflow presents a list of emoji, so naturally the icon for each
    list item would be that emoji. Problem is, Alfred only supports files
    for this.
    
    What we can do is grab the pngs we need from EmojiOne. I'm having them
    written to the data directory, which seems better than just packaging
    them in the workflow?

--- gitmoji.py
+++ gitmoji.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # encoding: utf-8
 
-import sys
+import sys, os
 
 from workflow import Workflow3, web
 
 gitmoji_db = 'https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json'
-
+emojione_assets = 'https://raw.githubusercontent.com/emojione/emojione-assets/master/png/128'
 
 def get_gitmoji_db():
     return web.get(gitmoji_db).json()
@@ -16,11 +16,22 @@ def main(wf):
     data = wf.cached_data('gitmoji_db', get_gitmoji_db, max_age=86400)
 
     for emoji in data['gitmojis']:
+        unicode = emoji['emoji']
+        str = unicode.encode('unicode-escape')
+        if str.startswith('\U000'):
+            id  = str[5:]
+        elif str.startswith('\u'):
+            id = str.split('\u')[1]
+        icon = wf.datafile(id + '.png')
+        if not os.path.isfile(icon):
+            request = web.get(emojione_assets + '/' + id + '.png')
+            request.save_to_path(icon)
+
         wf.add_item(
             title=emoji['emoji'] + ' ' + emoji['code'],
             subtitle=emoji['description'],
             valid=True,
-            icon='9B7FE8AC-6582-4825-917E-92D0E0291CC1.png',
+            icon=icon,
             match=emoji['name'] + ' ' + emoji['description'],
             arg=emoji['code'],
             copytext=emoji['code'],
