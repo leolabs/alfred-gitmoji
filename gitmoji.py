e62f0ba4ab6b805345ae1e489af57e0bfcb4f6cb
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-02-23 17:02:45 -0500

    🐛  Address issue with Label emoji
    The new gitmoji for "Adding or updating types" introduces a new case
    where the emoji being used decodes to a combination of \U and \u escaped
    codes (4 hex and 8 hex respectively), so the logic needs to improve in
    order to correctly resolve to the filename used by emojione-assets.

--- gitmoji.py
+++ gitmoji.py
@@ -7,6 +7,7 @@ from workflow import Workflow3, web
 
 gitmoji_db = 'https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json'
 emojione_assets = 'https://raw.githubusercontent.com/emojione/emojione-assets/master/png/128'
+esc_seqs = {'\\U000': ' ', '\\u': ' '}
 
 def get_gitmoji_db():
     return web.get(gitmoji_db).json()
@@ -16,15 +17,15 @@ def main(wf):
     data = wf.cached_data('gitmoji_db', get_gitmoji_db, max_age=86400)
 
     for emoji in data['gitmojis']:
-        unicode = emoji['emoji']
-        str = unicode.encode('unicode-escape')
-        if str.startswith('\U000'):
-            id  = str[5:]
-        elif str.startswith('\u'):
-            id = str.split('\u')[1]
-        icon = wf.datafile(id + '.png')
+        unicode_emoji = emoji['emoji']
+        ascii_emoji = unicode_emoji.encode('unicode-escape')
+        for r,s in esc_seqs.items():
+            ascii_emoji = ascii_emoji.replace(r,s)
+        codes = ascii_emoji.split()
+        filename = codes[0]
+        icon = wf.datafile(filename + '.png')
         if not os.path.isfile(icon):
-            request = web.get(emojione_assets + '/' + id + '.png')
+            request = web.get(emojione_assets + '/' + filename + '.png')
             request.save_to_path(icon)
 
         wf.add_item(
